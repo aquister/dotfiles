d9b3226890447e9fd0ab15a1dbd9c3cae6ebc37d
aquister <alec90@gmail.com>
2015-01-20 23:29:11 +0100

    yes yes

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_logout bash_profile bashrc htoprc livestreamerrc vimrc"    # list of files/folders to symlink in homedir
+files="bash_logout bash_profile bashrc htoprc livestreamerrc vimrc xinit Xresources gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
