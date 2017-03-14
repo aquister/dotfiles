3e02821bcdd93417decde5a191a226f27c3dea84
alelindq <alelindq@cisco.com>
2017-03-14 10:13:26 +0100

    simplify makesymlinks script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,32 +3,25 @@
 # dotfiles directory
 dir=~/dotfiles
 
-# old dotfiles backup directory
-olddir=~/dotfiles_old
+filelist=(
+  # xinitrc
+  # Xresources
+  bash_profile
+  bashrc
+  bash_logout
+  git-prompt.sh
+  gitconfig
+  vimrc
+  rpmmacros
+  tmux.conf
+  livestreamerrc
+  rtorrent.rc
+  # config/i3/config
+  # weechat
+)
 
-# list of files/folders to symlink in homedir
-files="config weechat \
-bash_logout bash_profile bashrc git-prompt.sh gitconfig \
-livestreamerrc vimrc rtorrent.rc rpmmacros \
-xinitrc Xresources tmux.conf"
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+for file in ${filelist[@]}; do
+  echo "Creating symlink to $file in home directory."
+  ln -sf $dir/$file ~/.$file
 done
 
